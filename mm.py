21ebbfbe9a63d002b24a137fb666a6a9307ad8d0
Lukas Slouka <xslouk02@stud.fit.vutbr.cz>
2015-12-09 19:56:19 +0100

    no output

--- mm.py
+++ mm.py
@@ -2,8 +2,12 @@
 import sys
 from mind_mapper import Base
 from mind_mapper.views import View
+import logging
+
 
 if __name__ == '__main__':
+    logging.disable(logging.DEBUG)
+    logging.disable(logging.INFO)
     proj = Base()
     view = View()
     view.run()
